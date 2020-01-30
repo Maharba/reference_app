d06b5d79c9f0300988d26d2666585d5038e1d902
Abraham Chacon Landois <maharbazster@gmail.com>
2020-01-30 09:25:08 -0500

    Fixed test

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -7,8 +7,7 @@
 
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
-
-import 'package:reference_app/main.dart';
+import 'package:reference_app/views/home.dart';
 
 void main() {
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
