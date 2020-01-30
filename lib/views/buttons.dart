de79d2150d4e945d922bc48ba42f5f1d70d22d87
Abraham Chacon Landois <maharbazster@gmail.com>
2020-01-29 23:39:42 -0500

    Added buttons

--- lib/views/buttons.dart
+++ lib/views/buttons.dart
@@ -16,10 +16,12 @@ class Buttons extends StatelessWidget {
           child: Column(
             mainAxisAlignment: MainAxisAlignment.center,
             children: <Widget>[
+
               RaisedButton(
                 child: Text('Raised Button'),
                 onPressed: () => print("You have pressed the RaisedButton.")
               ),
+              
               DropdownButton(
                 items: [
                   DropdownMenuItem(
@@ -31,7 +33,44 @@ class Buttons extends StatelessWidget {
                 ],
                 onChanged: (param) => print("Since this is a Stateless Widget, it cannot change the state of the current view." + 
                 "Make this a Statefull Widget in order to modify the UI.")
-              )
+              ),
+
+              FlatButton(
+                textColor: Colors.blue,
+                child: Text("FlattButton"), 
+                onPressed: () => print("You have pressed the FlattButton")
+              ),
+
+              ButtonBar(
+                children: <Widget>[
+                  FlatButton(
+                    child: Text("Skip"),
+                    onPressed: () => print("object"),
+                  ),
+                  RaisedButton(
+                    child: Text("Continue"),
+                    onPressed: () => print("object"),
+                  ),
+                ],
+              ),
+
+              FloatingActionButton(
+                child: Icon(Icons.navigation),
+                onPressed: () => print("You pressed the FAB."),
+              ),
+
+              IconButton(
+                color: Colors.red,
+                icon: Icon(Icons.favorite),
+                onPressed: () => print("You pressed the IconButton  "),
+              ),
+
+              OutlineButton(
+                child: Text("OutlineButton"),
+                onPressed: () => print("You pressed the OutlineButton"),
+              ),
+
+              
             ],
           )
         )
